dd318af9d8182f80538536d0077947e043275a97
Paul Mucur <mudge@mudge.name>
2020-04-25 20:52:52 +0100

    Use new, unminified JavaScript
    After @chastell pointed out that the compressed JavaScript already fits
    inside a single TCP packet, it seems pointless to minify it.
    
    While we're switching to the unminified version, play a little code golf
    by simplifying how the distance of time in words is calculated and port
    all the tests from the original Rails version into a corresponding Jest
    test suite.
    
    See https://github.com/rails/rails/blob/6-0-stable/actionview/test/template/date_helper_test.rb#L25
    
    The big thing this flagged is that JavaScript Date#getYear() returns "a
    number representing the year of the given date, according to local time,
    minus 1900" and _not_ the full year as I expected. This played havoc
    with my leap year calculations so switch to Date#getFullYear() which
    gives us what I originally expected.
    
    Thanks to https://stackoverflow.com/a/4587611 for giving me a simple way
    to calculate the number of leap years between any two years without
    having to use a loop to check each year individually.

---
+++ j/2.js
@@ -0,0 +1,80 @@
+function distanceOfTimeInWords(from, to) {
+  var milliseconds = to - from,
+      seconds = Math.round(milliseconds / 1000),
+      minutes = Math.round(seconds / 60),
+      fromYear = from.getFullYear(),
+      toYear = to.getFullYear(),
+      leapYears, leapDayMinutes, minutesWithoutLeapDays, years, remainingMinutes;
+
+  if (minutes === 0) {
+    return "less than a minute";
+  }
+
+  if (minutes === 1) {
+    return "1 minute";
+  }
+
+  if (minutes < 45) {
+    return minutes + " minutes";
+  }
+
+  if (minutes < 90) {
+    return "about 1 hour";
+  }
+
+  if (minutes < 1440) {
+    return "about " + Math.round(minutes / 60) + " hours";
+  }
+
+  if (minutes < 2520) {
+    return "1 day";
+  }
+
+  if (minutes < 43200) {
+    return Math.round(minutes / 1440) + " days";
+  }
+
+  if (minutes < 64800) {
+    return "about 1 month";
+  }
+
+  if (minutes < 86400) {
+    return "about " + Math.round(minutes / 43200) + " months";
+  }
+
+  if (minutes < 525600) {
+    return Math.round(minutes / 43200) + " months";
+  }
+
+  if (from.getMonth() >= 2) {
+    fromYear++;
+  }
+
+  if (to.getMonth() < 2) {
+    toYear--;
+  }
+
+  toYear--;
+
+  leapYears = (Math.floor(toYear / 4) - Math.floor(toYear / 100) + Math.floor(toYear / 400)) -
+    (Math.floor(fromYear / 4) - Math.floor(fromYear / 100) + Math.floor(fromYear / 400));
+
+  leapDayMinutes = leapYears * 1440;
+  minutesWithoutLeapDays = minutes - leapDayMinutes;
+  remainingMinutes = minutesWithoutLeapDays % 525600;
+  years = Math.floor(minutesWithoutLeapDays / 525600);
+
+  if (remainingMinutes < 131400) {
+    return "about " + years + " year" + (years === 1 ? "" : "s");
+  }
+
+  if (remainingMinutes < 394200) {
+    return "over " + years + " year" + (years === 1 ? "" : "s");
+  }
+
+  return "almost " + (years + 1) + " years";
+}
+
+if (typeof module === "object" && module.exports) {
+  module.exports = distanceOfTimeInWords;
+}
---
+++ j/2.test.js
@@ -0,0 +1,295 @@
+const distanceOfTimeInWords = require("./2");
+
+test("less than a minute", () => {
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-01-01T00:00:00Z")
+    )
+  ).toBe("less than a minute");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-01-01T00:00:29Z")
+    )
+  ).toBe("less than a minute");
+});
+
+test("1 minute", () => {
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-01-01T00:00:30Z")
+    )
+  ).toBe("1 minute");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-01-01T00:01:29Z")
+    )
+  ).toBe("1 minute");
+});
+
+test("2 minutes up to 45 minutes", () => {
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-01-01T00:01:30Z")
+    )
+  ).toBe("2 minutes");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-01-01T00:44:29Z")
+    )
+  ).toBe("44 minutes");
+});
+
+test("45 minutes up to 90 minutes", () => {
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-01-01T00:44:30Z")
+    )
+  ).toBe("about 1 hour");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-01-01T01:29:29Z")
+    )
+  ).toBe("about 1 hour");
+});
+
+test("90 minutes up to 24 hours", () => {
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-01-01T01:29:30Z")
+    )
+  ).toBe("about 2 hours");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-01-01T23:59:29Z")
+    )
+  ).toBe("about 24 hours");
+});
+
+test("24 hours up to 42 hours", () => {
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-01-01T23:59:30Z")
+    )
+  ).toBe("1 day");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-01-02T17:59:29Z")
+    )
+  ).toBe("1 day");
+});
+
+test("42 hours up to 30 days", () => {
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-01-02T17:59:30Z")
+    )
+  ).toBe("2 days");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-01-03T12:00:00Z")
+    )
+  ).toBe("3 days");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-01-30T23:59:29Z")
+    )
+  ).toBe("30 days");
+});
+
+test("30 days up to 60 days", () => {
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-01-30T23:59:30Z")
+    )
+  ).toBe("about 1 month");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-02-14T23:59:29Z")
+    )
+  ).toBe("about 1 month");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-02-14T23:59:30Z")
+    )
+  ).toBe("about 2 months");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-02-29T23:59:29Z")
+    )
+  ).toBe("about 2 months");
+});
+
+test("60 days up to 365 days", () => {
+  expect(
+    distanceOfTimeInWords(
+      new Date("2000-01-01T00:00:00Z"),
+      new Date("2000-02-29T23:59:30Z")
+    )
+  ).toBe("2 months");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2004-06-06T21:45:00Z"),
+      new Date("2005-06-06T21:44:29Z")
+    )
+  ).toBe("12 months");
+});
+
+test("over 365 days", () => {
+  expect(
+    distanceOfTimeInWords(
+      new Date("2004-06-06T21:45:00Z"),
+      new Date("2005-06-06T21:44:30Z")
+    )
+  ).toBe("about 1 year");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2004-06-06T21:45:00Z"),
+      new Date("2005-09-05T21:45:00Z")
+    )
+  ).toBe("about 1 year");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2004-06-06T21:45:00Z"),
+      new Date("2005-12-06T21:45:00Z")
+    )
+  ).toBe("over 1 year");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2004-06-06T21:45:00Z"),
+      new Date("2006-03-07T21:45:00Z")
+    )
+  ).toBe("almost 2 years");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2004-06-06T21:45:00Z"),
+      new Date("2006-09-05T21:45:00Z")
+    )
+  ).toBe("about 2 years");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2004-06-06T21:45:00Z"),
+      new Date("2006-09-07T21:45:00Z")
+    )
+  ).toBe("over 2 years");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2004-06-06T21:45:00Z"),
+      new Date("2007-03-05T21:45:00Z")
+    )
+  ).toBe("over 2 years");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2004-06-06T21:45:00Z"),
+      new Date("2007-03-07T21:45:00Z")
+    )
+  ).toBe("almost 3 years");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2004-06-06T21:45:00Z"),
+      new Date("2009-03-07T21:45:00Z")
+    )
+  ).toBe("almost 5 years");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2004-06-06T21:45:00Z"),
+      new Date("2009-09-05T21:45:00Z")
+    )
+  ).toBe("about 5 years");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2004-06-06T21:45:00Z"),
+      new Date("2009-09-07T21:45:00Z")
+    )
+  ).toBe("over 5 years");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2004-06-06T21:45:00Z"),
+      new Date("2010-03-05T21:45:00Z")
+    )
+  ).toBe("over 5 years");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2004-06-06T21:45:00Z"),
+      new Date("2010-03-07T21:45:00Z")
+    )
+  ).toBe("almost 6 years");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2004-06-06T21:45:00Z"),
+      new Date("2014-03-07T21:45:00Z")
+    )
+  ).toBe("almost 10 years");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2004-06-06T21:45:00Z"),
+      new Date("2014-09-05T21:45:00Z")
+    )
+  ).toBe("about 10 years");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2004-06-06T21:45:00Z"),
+      new Date("2014-09-07T21:45:00Z")
+    )
+  ).toBe("over 10 years");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2004-06-06T21:45:00Z"),
+      new Date("2015-03-05T21:45:00Z")
+    )
+  ).toBe("over 10 years");
+
+  expect(
+    distanceOfTimeInWords(
+      new Date("2004-06-06T21:45:00Z"),
+      new Date("2015-03-07T21:45:00Z")
+    )
+  ).toBe("almost 11 years");
+});
+
